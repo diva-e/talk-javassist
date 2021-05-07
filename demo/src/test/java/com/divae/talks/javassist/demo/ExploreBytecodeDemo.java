1c01278a76ddbccc6401410b74775a6fffef4803
Christian Kumpe <christian.kumpe@diva-e.com>
2021-05-07 13:42:56 +0200

    Improve demo code a little bit

--- demo/src/test/java/com/divae/talks/javassist/demo/ExploreBytecodeDemo.java
+++ demo/src/test/java/com/divae/talks/javassist/demo/ExploreBytecodeDemo.java
@@ -14,29 +14,29 @@ class ExploreBytecodeDemo {
 
     @Test
     void exploreClass() throws Exception {
-
         // fetch ClassPool
         ClassPool classPool = ClassPool.getDefault();
 
         // load CtClass
         CtClass ctClass = classPool.get("com.divae.talks.javassist.demo.ExploreBytecodeDemo$Point");
 
-        System.out.println("## The contains the following fields:");
+        System.out.println("## The class contains the following fields:");
         for (CtField ctField : ctClass.getDeclaredFields()) {
             System.out.println(ctField);
         }
 
-        System.out.println("## Opcodes of toString method:");
+        System.out.println("## Opcodes of the toString method:");
         CtMethod ctMethod = ctClass.getDeclaredMethod("toString");
         MethodInfo methodInfo = ctMethod.getMethodInfo();
         CodeAttribute codeAttribute = methodInfo.getCodeAttribute();
+
         CodeIterator codeIterator = codeAttribute.iterator();
         while (codeIterator.hasNext()) {
             int index = codeIterator.next();
             int op = codeIterator.byteAt(index);
-            System.out.println(Mnemonic.OPCODE[op]);
+            String opcodeName = Mnemonic.OPCODE[op];
+            System.out.printf("Index at %2d: %s\n", index, opcodeName);
         }
-
     }
 
     public static class Point {
--- demo/src/test/java/com/divae/talks/javassist/demo/ModifyBytecodeDemo.java
+++ demo/src/test/java/com/divae/talks/javassist/demo/ModifyBytecodeDemo.java
@@ -10,6 +10,19 @@ import static org.hamcrest.MatcherAssert.assertThat;
 
 class ModifyBytecodeDemo {
 
+    @Test
+    void createClass() throws Exception {
+        ClassPool classPool = ClassPool.getDefault();
+        CtClass ctClass = classPool.makeClass("com.divae.talks.javassist.demo.SyntheticClass");
+
+        CtConstructor ctConstructor = new CtConstructor(new CtClass[0], ctClass);
+        ctClass.addConstructor(ctConstructor);
+        ctConstructor.setBody("System.out.println(\"I'm the constructor\");");
+
+        Object instance = ctClass.toClass().getDeclaredConstructor().newInstance();
+        System.out.println(instance);
+    }
+
     /**
      * <em>Hint:</em> In order to successfully execute this test you need to switch
      * the fork mode in test execution of IntelliJ to method!
@@ -35,12 +48,12 @@ class ModifyBytecodeDemo {
         }
 
         Entity entity = (Entity) ctClass.toClass().getDeclaredConstructor().newInstance();
-        entity.setValue(123);
+        entity.setValue(21);
 
         assertThat(entity.getValue(), is(42));
         assertThat(entity.toString(), is("Entity with value=42"));
+        // assertThat(entity.value, is(??));
     }
-
 
     static class Entity {
         private int value;
